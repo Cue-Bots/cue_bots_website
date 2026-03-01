bf7f6866a6a72c02d2b4f488ade8a47fa4430557
lucasfontana <lucasfontana067@gmail.com>
2026-03-01 20:56:31 +0100

    [ADD] route

--- src/App.js
+++ src/App.js
@@ -1,50 +1,69 @@
-import './App.css';
-import NavBar from './components/nav_bar';
-import FirstPage from './components/fisrt_page';
-import WaveGrid from './components/WaveGrid';
-import Describe from './components/describe';
-import Partner from './components/Partner';
-import Newletter from './components/Newletter';
-import Contact from './components/Contact';
+// import './css/App.css';
+// import NavBar from './components/nav_bar';
+// import FirstPage from './components/fisrt_page';
+// import WaveGrid from './components/WaveGrid';
+// import Describe from './components/describe';
+// import Partner from './components/Partner';
+// import Newletter from './components/Newletter';
+// import Contact from './components/Contact';
 
-document.addEventListener('mousemove', (e) => {
-  const x = (e.clientX / window.innerWidth - 0.5) * 10;
-  const y = (e.clientY / window.innerHeight - 0.5) * 10;
+// document.addEventListener('mousemove', (e) => {
+//   const x = (e.clientX / window.innerWidth - 0.5) * 10;
+//   const y = (e.clientY / window.innerHeight - 0.5) * 10;
 
-  const bg = document.querySelector('.DynamicBackground');
-  if (bg) {
-    bg.style.transform = `translate(${x}%, ${y}%) scale(1.05)`;
-  }
-});
+//   const bg = document.querySelector('.DynamicBackground');
+//   if (bg) {
+//     bg.style.transform = `translate(${x}%, ${y}%) scale(1.05)`;
+//   }
+// });
 
+
+// function App() {
+//   return (
+//     <div id="acceuil" className="App">
+//       <header className="App-header">
+//         <section className="section dark-bg">
+//         <NavBar />
+//         <FirstPage />
+//         </section>
+//       </header>
+//       <div className="BackgroundWrapper">
+//         <WaveGrid />
+//       </div>
+//       <section className="section light-bg">
+//         <Describe />
+//       </section>
+//       <section className="section dark-bg">
+//         <Partner />
+//       </section>
+//       <section className="section dark-bg">
+//         <Newletter />
+//       </section>
+//       <section className="section dark-bg">
+//         <Contact />
+//       </section>
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import Products from "./pages/Products";
 
 function App() {
   return (
-    <div id="acceuil" className="App">
-      <header className="App-header">
-        <section className="section dark-bg">
-        <NavBar />
-        <FirstPage />
-        </section>
-      </header>
-      <div className="BackgroundWrapper">
-        <WaveGrid />
-      </div>
-      <section className="section light-bg">
-        <Describe />
-      </section>
-      <section className="section dark-bg">
-        <Partner />
-      </section>
-      <section className="section dark-bg">
-        <Newletter />
-      </section>
-      <section className="section dark-bg">
-        <Contact />
-      </section>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
   );
 }
 
-
 export default App;
+
---
+++ src/pages/Products.js
@@ -0,0 +1,50 @@
+import '../styles/Products.css';
+import NavBar from '../components/nav_bar';
+import FirstPage from '../components/fisrt_page';
+import WaveGrid from '../components/WaveGrid';
+import Describe from '../components/describe';
+import Partner from '../components/Partner';
+import Newletter from '../components/Newletter';
+import Contact from '../components/Contact';
+
+document.addEventListener('mousemove', (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 10;
+  const y = (e.clientY / window.innerHeight - 0.5) * 10;
+
+  const bg = document.querySelector('.DynamicBackground');
+  if (bg) {
+    bg.style.transform = `translate(${x}%, ${y}%) scale(1.05)`;
+  }
+});
+
+
+function Products() {
+  return (
+    <div id="acceuil" className="App">
+      <header className="App-header">
+        <section className="section dark-bg">
+        <NavBar />
+        <FirstPage />
+        </section>
+      </header>
+      <div className="BackgroundWrapper">
+        <WaveGrid />
+      </div>
+      <section className="section light-bg">
+        <Describe />
+      </section>
+      <section className="section dark-bg">
+        <Partner />
+      </section>
+      <section className="section dark-bg">
+        <Newletter />
+      </section>
+      <section className="section dark-bg">
+        <Contact />
+      </section>
+    </div>
+  );
+}
+
+
+export default Products;
